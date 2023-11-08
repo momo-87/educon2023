3a60263cc112287b9968d2c4a6fb11cf726c3deb
Momo-87 <momochristian87@gmail.com>
2023-11-08 22:20:18 +0100

    Implement styling for active nav menu item

--- scripts/script.js
+++ scripts/script.js
@@ -113,10 +113,16 @@ moreSpeakersButton.addEventListener('click', () => {
   moreSpeakersButton.style.display = 'none';
 });
 
-// Add evenListener to toggle the active status of a navbar menu item
-const navItems = document.querySelectorAll('navItem');
-navItems.forEach((item) => {
-  item.addEventListener('click', () => {
-    item.classList.toggle('active');
+// Add eventListener to toggle the active status of a navbar menu item
+const navLinks = document.querySelectorAll('.navLink');
+let currentLink = navLinks[0];
+navLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    if (e.target.innerHTML !== currentLink.innerHTML) {
+      const prevLink = currentLink;
+      prevLink.classList.toggle('active');
+      currentLink = e.target;
+      currentLink.classList.toggle('active');
+    }
   });
 });
