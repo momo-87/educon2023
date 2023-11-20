b36138da3bbadb289d6075b0a2637e2f80f84249
Momo-87 <momochristian87@gmail.com>
2023-11-20 13:14:13 +0100

    Added evenListener to create and select the lunch for the selected date

--- scripts/join.js
+++ scripts/join.js
@@ -12,14 +12,63 @@ navLinks.forEach((link) => {
   });
 });
 
-// Add eventListener to toggle the active status of a selected price
-// const prices = document.querySelectorAll('.price');
-// prices.forEach((price) => {
-//   price.addEventListener('click', (e) => {
-//     e.target.classList.toggle('selected-price');
-//   });
-// });
-
 //  Create and render user lunch selction list
-const li = document.createElement('li');
-const ul = document.querySelector('.lunch-selection');
+const div2 = document.createElement('div'); // second div in the li element
+
+// Creating input (radio button) for each menu
+const menus = ['menu1', 'menu2', 'menu3', 'menu4', 'menu5']; // menu options
+
+menus.forEach((menu) => {
+  const span = document.createElement('span');
+  const label = document.createElement('label');
+  label.setAttribute('for', menu);
+  label.textContent = menu;
+
+  const input = document.createElement('input');
+  input.setAttribute('type', 'radio');
+  input.setAttribute('name', 'menu');
+  input.setAttribute('value', menu);
+
+  span.append(label, input);
+  div2.appendChild(span);
+});
+
+// Render the content for the selected date
+const days = ['Thursday 13th of April 2023', 'Friday 14th of April 2023']; // Conference days options
+const inputValues = ['day-1-norm', 'day-2-norm', 'day-1-std', 'day-2-std']; // single day selection options
+const inputs = document.querySelectorAll('input'); // all input elements (radio button) in the select ticket section
+const ul = document.querySelector('.lunch-selection'); // ul element to append the li elements
+
+inputs.forEach((input) => {
+  input.addEventListener('click', (e) => {
+    // Remove previous li elements
+    while (ul.firstChild) {
+      ul.removeChild(ul.firstChild);
+    }
+
+    if (inputValues.includes(e.target.value)) {
+      const li = document.createElement('li');
+      const div1 = document.createElement('div');
+
+      if (e.target.value === 'day-1-norm' || e.target.value === 'day-1-std') {
+        const firstDay = days[0];
+        div1.innerText = firstDay;
+      } else {
+        const secondDay = days[1];
+        div1.innerText = secondDay;
+      }
+      li.append(div1, div2);
+      ul.appendChild(li);
+    } else {
+      days.forEach((day) => {
+        const li = document.createElement('li');
+        const div1 = document.createElement('div');
+        div1.innerText = day;
+
+        const div2Clone = div2.cloneNode(true); // Create a new instance of div2 for each li
+        li.append(div1, div2Clone);
+        ul.appendChild(li);
+      });
+    }
+  });
+});
